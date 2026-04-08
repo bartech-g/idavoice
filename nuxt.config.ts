516455b6f03b9bd78f4509b01406b17678d1d0d4
bartech-g <bartech.js@gmail.com>
2026-04-08 18:04:52 +0200

    fuck zod bullshit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import tailwindcss from "@tailwindcss/vite";
-import "./shared/schemas/env";
+// import "./lib/schemas/env";
 
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
--- server/db/index.ts
+++ server/db/index.ts
@@ -1,6 +1,6 @@
 import { drizzle } from 'drizzle-orm/neon-http';
 import { neon } from '@neondatabase/serverless';
-import env from '../../shared/schemas/env';
+import env from '../../lib/schemas/env';
 
 const sql = neon(env.DATABASE_URL!);
 const db = drizzle(sql, { casing: "snake_case" });
